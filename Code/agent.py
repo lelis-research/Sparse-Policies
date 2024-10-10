77f130659cf4170231786fedebbc0cdeec6fc7b0
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2024-10-10 17:48:49 -0600

    added args to agent.py

--- Code/agent.py
+++ Code/agent.py
@@ -2,9 +2,9 @@ import copy
 import random
 import torch
 import numpy as np
-
-from combo import Game
+from environment.combo import Game
 from models.model import CustomRNN, CustomRelu 
+import argparse
 
 class Trajectory:
     def __init__(self):
@@ -185,45 +185,64 @@ class PolicyGuidedAgent:
         return trajectory
 
 def main():
-    hidden_size = 32
-    game_width = 3
-    lambda_l1 = 0.001
-    rnn = CustomRelu(game_width**2 * 2 + 9, hidden_size, 3, lambda_l1=lambda_l1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_model', default="nn", type=str)
+    parser.add_argument('--game_width', default=5, type=int)
+    parser.add_argument('--hidden_size', default=32, type=int)
+    parser.add_argument('--l1', default=0.001, type=float)
+    parser.add_argument('--problem', default="All", type=str)
+    parser.add_argument('--lr', default=0.1, type=float)
+    parser.add_argument('--log_path', default="logs/", type=str)
 
-    policy_agent = PolicyGuidedAgent()
 
-    problem = "TL-BR"
-    # problem = "TR-BL"
-    # problem = "BR-TL"
-    # problem = "BL-TR"
+    args = parser.parse_args()
 
-    shortest_trajectory_length = np.inf
-    best_trajectory = None
+    if args.base_model == "nn":
 
-    for _ in range(150):
-        for _ in range(500):
+        policy_agent = PolicyGuidedAgent()
+
+        if args.problem == "All":
+            problems = ["TL-BR", "TR-BL", "BR-TL", "BL-TR"]
+        else:
+            problems = [args.problem]
+
+        for problem in problems:
+            print("Problem: ", problem)
+
+            hidden_size = args.hidden_size
+            game_width = args.game_width
+            lambda_l1 = args.l1
+            learning_rate = args.lr
+
+            rnn = CustomRelu(game_width**2 * 2 + 9, hidden_size, 3, lambda_l1=lambda_l1)
+
+            shortest_trajectory_length = np.inf
+            best_trajectory = None
+
+            for _ in range(150):
+                for _ in range(500):
+                    env = Game(game_width, game_width, problem)
+                    trajectory = policy_agent.run(env, rnn, length_cap=shortest_trajectory_length, verbose=False)
+
+                    if len(trajectory.get_trajectory()) < shortest_trajectory_length:
+                        shortest_trajectory_length = len(trajectory.get_trajectory())
+                        best_trajectory = trajectory
+
+                print('Trajectory length: ', len(best_trajectory.get_trajectory()))
+                for _ in range(10):
+                    loss = rnn.train(best_trajectory)
+                    print(loss)
+                print()
+
+            policy_agent._epsilon = 0.0
             env = Game(game_width, game_width, problem)
-            trajectory = policy_agent.run(env, rnn, length_cap=shortest_trajectory_length, verbose=False)
+            policy_agent.run(env, rnn, greedy=True, length_cap=None, verbose=True)
+            rnn.print_weights()
 
-            if len(trajectory.get_trajectory()) < shortest_trajectory_length:
-                shortest_trajectory_length = len(trajectory.get_trajectory())
-                best_trajectory = trajectory
+            env = Game(game_width, game_width, problem)
+            policy_agent.run_with_relu_state(env, rnn)
 
-        print('Trajectory length: ', len(best_trajectory.get_trajectory()))
-        for _ in range(10):
-            loss = rnn.train(best_trajectory)
-            print(loss)
-        print()
-
-    policy_agent._epsilon = 0.0
-    env = Game(game_width, game_width, problem)
-    policy_agent.run(env, rnn, greedy=True, length_cap=None, verbose=True)
-    rnn.print_weights()
-
-    env = Game(game_width, game_width, problem)
-    policy_agent.run_with_relu_state(env, rnn)
-
-    torch.save(rnn.state_dict(), 'binary/game-width' + str(game_width) + '-' + problem + '-relu-' + str(hidden_size) + '-l1-' + str(lambda_l1) + '-model.pth')
+            torch.save(rnn.state_dict(), 'binary/NN-game-width' + str(game_width) + '-' + problem + '-relu-' + str(hidden_size) + '-l1-' + str(lambda_l1) + '-lr-' + str(learning_rate) + '-model.pth')
 
 if __name__ == "__main__":
     main()
