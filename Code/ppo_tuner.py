fe0dde9088dcdca6937b603b315d06fe29a619b1
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2024-10-24 13:57:00 -0600

    added args to it

--- Code/ppo_tuner.py
+++ Code/ppo_tuner.py
@@ -2,27 +2,28 @@ import optuna
 from tuner import Tuner 
 
 tuner = Tuner(
-    script="/Users/amirhosseinrajabpour/Documents/UofA/Thesis/neural-policy-decomposition-main/code/train_ppo.py --env_id ComboGrid_TL-BR --game_width 3 --hidden_size 32 --log_path ppo_logs/combogrid/logfile",
-    metric="charts/episodic_length",
+    script="/Users/amirhosseinrajabpour/Documents/UofA/Thesis/neural-policy-decomposition-main/code/train_ppo.py --env_id ComboGrid_TL-BR",
+    metric="charts/episodic_return",
     metric_last_n_average_window=50,
-    direction="minimize",
+    direction="maximize",
     aggregation_type="average",
     target_scores={
-        "ComboGrid_TL-BR": [0, 500],
+        "ComboGrid_TL-BR": [-500, 0],
         # "ComboGrid_TR-BL": [0, 500],
         # "ComboGrid_BR-TL": [0, 500],
         # "ComboGrid_BL-TR": [0, 500]
     },
     params_fn=lambda trial: {
-        # "learning-rate": trial.suggest_float("learning-rate", 0.0003, 0.003, log=True),
-        # "num-minibatches": trial.suggest_categorical("num-minibatches", [1, 2, 4]),
-        # "update-epochs": trial.suggest_categorical("update-epochs", [1, 2, 4, 8]),
-        # "num-steps": trial.suggest_categorical("num-steps", [5, 16, 32, 64, 128]),
-        # "vf-coef": trial.suggest_float("vf-coef", 0, 5),
-        # "max-grad-norm": trial.suggest_float("max-grad-norm", 0, 5),
-
-        "total-timesteps": 50000,
-        "num-envs": 16,
+        # "env_id": "ComboGrid_TL-BR",
+        "total-timesteps": 100000,
+        "num-envs": 4,
+        "game_width": 3,
+        "hidden_size": 32,
+        "log_path": "ppo_logs/combogrid/logfile",
+        "options_num_epochs": 5000,
+        "options_l1_lambda": 0.0005,
+        "options_learning_rate": 0.1,
+        "exp_name": "ComboGrid_TL-BR_optuna_test",
 
         "learning_rate": trial.suggest_categorical("learning_rate", [0.005, 0.001, 0.0005, 0.0001, 0.00005]),
         "clip_coef": trial.suggest_categorical("clip_coef", [0.1, 0.15, 0.2, 0.25, 0.3]),
