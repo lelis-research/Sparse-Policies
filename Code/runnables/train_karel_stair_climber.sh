7dfa0a833e1bf6b09e0158876df86ff390755d6b
Amirhossein Rajabpour <amirrj@beluga3.int.ets1.calculquebec.ca>
2024-11-15 23:32:18 -0500

    output folder changed

--- Code/runnables/train_karel_stair_climber.sh
+++ Code/runnables/train_karel_stair_climber.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=32   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=16G        # memory per node
 #SBATCH --time=2-00:00      # time (DD-HH:MM)
-#SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+#SBATCH --output=job_logs/%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --account=def-lelis
 #SBATCH --mail-user=arajabpo@ualberta.ca
 #SBATCH --mail-type=ALL
